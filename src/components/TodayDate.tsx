e7c38d3f05a7d35d3c3ff17485da8d10f16876a0
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-06-01 20:05:32 +0530

    set null on server

--- src/components/TodayDate.tsx
+++ src/components/TodayDate.tsx
@@ -4,12 +4,12 @@ import { format } from "date-fns"
 import { useState, useEffect } from "react"
 
 export default function TodayDate() {
+  const [date, setDate] = useState<string | null>(null)
+  const [time, setTime] = useState<string | null>(null)
+
   const today = new Date()
   const formattedDate = format(today, "EEEE, MMMM do, yyyy")
   const formattedTime = format(today, "HH:mm a")
-
-  const [date, setDate] = useState(formattedDate)
-  const [time, setTime] = useState(formattedTime)
 
   useEffect(() => {
     setDate(formattedDate)
@@ -17,9 +17,11 @@ export default function TodayDate() {
   }, [formattedDate, formattedTime])
 
   return (
-    <div className="grid gap-4">
-      <p className="text-2xl font-medium text-zinc-300">{date}</p>
-      <p className="text-2xl font-medium text-zinc-300">{time}</p>
+    <div className="grid justify-center gap-4">
+      <p className="relative text-2xl font-medium text-zinc-300">
+        {date ?? "-- -- --"}
+      </p>
+      <p className="text-2xl font-medium text-zinc-300">{time ?? "-- -- --"}</p>
     </div>
   )
 }
