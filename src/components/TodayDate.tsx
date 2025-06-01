e961099449813ab38de497e1a664d73c2f0f67bc
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-06-01 19:33:29 +0530

    useEffect to render after the first render on client

--- src/components/TodayDate.tsx
+++ src/components/TodayDate.tsx
@@ -1,26 +1,25 @@
 "use client"
 
 import { format } from "date-fns"
-// import { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 
 export default function TodayDate() {
   const today = new Date()
   const formattedDate = format(today, "EEEE, MMMM do, yyyy")
   const formattedTime = format(today, "HH:mm a")
 
-  // const [date, setDate] = useState(formattedDate)
+  const [date, setDate] = useState(formattedDate)
+  const [time, setTime] = useState(formattedTime)
 
-  // useEffect(() => setDate(formattedDate), [formattedDate])
+  useEffect(() => {
+    setDate(formattedDate)
+    setTime(formattedTime)
+  }, [formattedDate, formattedTime])
 
   return (
     <div className="grid gap-4">
-      <p className="text-2xl font-medium text-zinc-300">{formattedDate}</p>
-      <p
-        suppressHydrationWarning={true}
-        className="text-2xl font-medium text-zinc-300"
-      >
-        {formattedTime}
-      </p>
+      <p className="text-2xl font-medium text-zinc-300">{date}</p>
+      <p className="text-2xl font-medium text-zinc-300">{time}</p>
     </div>
   )
 }
