99af83f92d8439eaa726b5cff419484bcbe0fb1d
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-06-01 18:05:53 +0530

    date with hours and mins

--- src/components/TodayDate.tsx
+++ src/components/TodayDate.tsx
@@ -6,10 +6,16 @@ import { format } from "date-fns"
 export default function TodayDate() {
   const today = new Date()
   const formattedDate = format(today, "EEEE, MMMM do, yyyy")
+  const formattedTime = format(today, "HH:mm a")
 
   // const [date, setDate] = useState(formattedDate)
 
   // useEffect(() => setDate(formattedDate), [formattedDate])
 
-  return <p className="text-2xl font-medium text-zinc-300">{formattedDate}</p>
+  return (
+    <div className="grid gap-4">
+      <p className="text-2xl font-medium text-zinc-300">{formattedDate}</p>
+      <p className="text-2xl font-medium text-zinc-300">{formattedTime}</p>
+    </div>
+  )
 }
